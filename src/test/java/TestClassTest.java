f5977ecf154359ce4d9e2ba83e692bb0fe4a03e5
Tuomas <tmaanonen@pm.me>
2020-04-01 15:46:58 +0300

    Added second test

--- src/test/java/TestClassTest.java
+++ src/test/java/TestClassTest.java
@@ -1,4 +1,3 @@
-import org.junit.Assert;
 import org.junit.Test;
 import Stuff.TestClass;
 import static org.assertj.core.api.Assertions.*;
@@ -12,6 +11,15 @@ public class TestClassTest {
         int n1n2 = 12;
 
         assertThat(TestClass.addition(n1, n2)).isEqualTo(n1n2);
+    }
+
+    @Test
+    public void multiplicationTest() {
+        int n1 = 5;
+        int n2 = 7;
+        int n1n2 = n1*n2;
+
+        assertThat(TestClass.addition(n1, n1)).isEqualTo(n1n2);
     }
 
 }
