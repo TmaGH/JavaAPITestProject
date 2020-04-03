1b1b79dba5dd558eb74d6b79ab965ad02d17ea68
Tuomas <tmaanonen@pm.me>
2020-04-03 10:46:24 +0300

    jacoco and static analysis test

--- src/main/java/Stuff/TestClass.java
+++ src/main/java/Stuff/TestClass.java
@@ -2,8 +2,11 @@ package Stuff;
 
 public class TestClass {
 
+    int test = 0;
+
     static public int addition(int n1, int n2) {
         return n1 + n2;
     }
     static public int multiplication(int n1, int n2) {return n1 * n2;}
+
 }
